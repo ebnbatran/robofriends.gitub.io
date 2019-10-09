3d872d16d7bdac921e5d91955b5144112860ab8e
ebnbatran <ebnbatran3@gmail.com>
2019-10-09 23:08:52 +0200

    Updates

---
+++ precache-manifest.4a35ab6c534099ae82232bf62fb50506.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b5ba5a05b914391d0bbf7791ef54c988",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "3c6d592831e33e629745",
+    "url": "/robofriends/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "d50ad4caf1db852a78c9",
+    "url": "/robofriends/static/css/main.5639cedd.chunk.css"
+  },
+  {
+    "revision": "3c6d592831e33e629745",
+    "url": "/robofriends/static/js/2.ebdd79c6.chunk.js"
+  },
+  {
+    "revision": "d50ad4caf1db852a78c9",
+    "url": "/robofriends/static/js/main.c2ed4b46.chunk.js"
+  },
+  {
+    "revision": "15646e7ae3f7e7d4e926",
+    "url": "/robofriends/static/js/runtime-main.59eca5ac.js"
+  },
+  {
+    "revision": "82e09f7862a1e26fb7dfe1d0f8adc87f",
+    "url": "/robofriends/static/media/SEGA.82e09f78.TTF"
+  }
+]);
